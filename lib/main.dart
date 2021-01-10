c72c8c8a3475adb01371db3658e87c6da71fea51
Shubham <coder_shubham@outlook.com>
2021-01-10 19:55:07 +0530

    Performance Optimization

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+// import 'package:flutter/services.dart';
 
 import './widgets/transaction_list.dart';
 import './widgets/new_transaction.dart';
